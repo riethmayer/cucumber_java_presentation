37ff3aed140268da9e874257792ed342be94e757
Jan Riethmayer <jan@riethmayer.de>
2010-10-19 00:04:57 +0200

    introduce project with title

---
+++ src/main/java/de/dailab/cucumber/Project.java
@@ -0,0 +1,16 @@
+package de.dailab.cucumber;
+
+public class Project{
+
+    String title;
+
+    /** Project constructor */
+    public Project(){}
+
+    void setTitle(String title){
+        this.title = title;
+    }
+    String getTitle(){
+        return this.title;
+    }
+}
