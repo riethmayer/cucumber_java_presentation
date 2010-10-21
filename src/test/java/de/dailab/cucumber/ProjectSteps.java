fcfe5d12b0eb94fe6ceee70ea34532a7ceb52984
Jan Riethmayer <jan@riethmayer.de>
2010-10-21 22:54:04 +0200

    add given step definitions for first scenario.

---
+++ src/test/java/de/dailab/cucumber/ProjectSteps.java
@@ -0,0 +1,14 @@
+package de.dailab.cucumber;
+
+import cuke4duke.annotation.Pending;
+import cuke4duke.annotation.I18n.EN.*;
+
+public class ProjectSteps{
+    /** ProjectSteps constructor */
+    public ProjectSteps(){}
+    @Given ("^A project with title \"([^\"]*)\"$")
+    public void aProjectWithTitle(String title) {
+        Project project = new Project();
+        project.setTitle(title);
+    }
+}
